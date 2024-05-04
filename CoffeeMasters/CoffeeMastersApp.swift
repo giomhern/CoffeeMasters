1f44fd1c400c12796e5afe5f86bdbaedf6a3dcc1
giovannimhern <giovannimhern@uchicago.edu>
2024-05-03 23:16:25 -0500

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Giovanni Maya on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMasters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeMasters
+//
+//  Created by Giovanni Maya on 5/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
