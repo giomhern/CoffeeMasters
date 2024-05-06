a8e8403f4950868d643d3a77d031dbec44d57a0d
giovannimhern <giovannimhern@uchicago.edu>
2024-05-05 21:55:11 -0500

    Final build of the app

--- CoffeeMasters/Page Views/OrderPage.swift
+++ CoffeeMasters/Page Views/OrderPage.swift
@@ -23,7 +23,7 @@ struct OrderPage: View {
                        ForEach(cartManager.cart, id:\.0.id) { item in
                            OrderItem(item: item)
                        }
-                   }.listRowBackground(Color("Background"))
+                   }.listRowBackground(Color("CardBackground"))
                                        
                    Section("YOUR DETAILS") {
                        VStack {
@@ -35,15 +35,15 @@ struct OrderPage: View {
                                .textFieldStyle(.roundedBorder)
                        }.padding(.top)
                         .padding(.bottom)
-                   }.listRowBackground(Color("Background"))
+                   }.listRowBackground(Color("CardBackground"))
                    
                    Section() {
                        HStack {
                            Spacer()
                            Text("Total")
                            Spacer()
-//                           Text("$ \($cartManager.total(), specifier: "%.2f")")
-//                               .bold()
+                           Text("$ \(cartManager.total(), specifier: "%.2f")")
+                               .bold()
                            Spacer()
                        }
                    }.listRowBackground(Color.clear)
